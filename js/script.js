c758a58608cdba7bcc5ea71f959434b87b0ce7f1
Krystian Gagracz <krystian.gagracz@ec-at.com>
2022-07-19 21:51:27 +0200

    formatting

--- js/script.js
+++ js/script.js
@@ -1,47 +1,50 @@
 window.addEventListener("DOMContentLoaded", () => {
-	const header = document.querySelector(".header");
+  const header = document.querySelector(".header");
 
-	//changing header color after scroll
-	window.addEventListener("scroll", () => {
-		if (window.scrollY > 200) {
-			header.style.background = "#1d282d";
-		} else {
-			header.style.background = "rgba(255, 255, 255, 0.05)";
-		}
-	});
+  //changing header color after scroll
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+      header.style.background = "#1d282d";
+    } else {
+      header.style.background = "rgba(255, 255, 255, 0.05)";
+    }
+  });
 
-	//mobile hamburger menu
-	const toggleNavButton = document.getElementById('toggle-nav');
-	const navContainer = document.getElementById('nav-container');
-	const navItems = [...document.querySelectorAll('.nav-item')];
-	const toggleButtonIcon = document.querySelector('#toggle-nav i')
+  //mobile hamburger menu
+  const toggleNavButton = document.getElementById("toggle-nav");
+  const navContainer = document.getElementById("nav-container");
+  const navItems = [...document.querySelectorAll(".nav-item")];
+  const toggleButtonIcon = document.querySelector("#toggle-nav i");
 
-	let mobileMenuActive = false;
-	const toggleNav = () => {
-		
-		if (!mobileMenuActive) {
-			toggleNavButton.classList.add('active');
-			navContainer.classList.add('active');
-			toggleButtonIcon.classList.replace('fa-bars', 'fa-x')			
-		} else {
-			toggleNavButton.classList.remove('active');
-			navContainer.classList.remove('active');
-			toggleButtonIcon.classList.replace('fa-x', 'fa-bars')			
-		}
-		mobileMenuActive = !mobileMenuActive;
-	}
-	
-	toggleNavButton.addEventListener('click', toggleNav);
-	navItems.forEach(navItem => {
-		navItem.addEventListener('click', toggleNav);
-	})
+  let mobileMenuActive = false;
+  const toggleNav = () => {
+    if (!mobileMenuActive) {
+      toggleNavButton.classList.add("active");
+      navContainer.classList.add("active");
+      toggleButtonIcon.classList.replace("fa-bars", "fa-x");
+    } else {
+      toggleNavButton.classList.remove("active");
+      navContainer.classList.remove("active");
+      toggleButtonIcon.classList.replace("fa-x", "fa-bars");
+    }
+    mobileMenuActive = !mobileMenuActive;
+  };
 
-	//show-slider-thumbs
-	const showSliderThumbsButton = document.getElementById('show-slider-thumbs');
-	const sliderThumbs = document.querySelector('.gallery-thumbs');
-	showSliderThumbsButton.addEventListener('click', () => {
-		if (sliderThumbs.classList.contains('active')) {sliderThumbs.classList.remove('active'); showSliderThumbsButton.classList.remove('active')}
-		else {sliderThumbs.classList.add('active');showSliderThumbsButton.classList.add('active')}
-	})
+  toggleNavButton.addEventListener("click", toggleNav);
+  navItems.forEach((navItem) => {
+    navItem.addEventListener("click", toggleNav);
+  });
 
+  //show-slider-thumbs
+  const showSliderThumbsButton = document.getElementById("show-slider-thumbs");
+  const sliderThumbs = document.querySelector(".gallery-thumbs");
+  showSliderThumbsButton.addEventListener("click", () => {
+    if (sliderThumbs.classList.contains("active")) {
+      sliderThumbs.classList.remove("active");
+      showSliderThumbsButton.classList.remove("active");
+    } else {
+      sliderThumbs.classList.add("active");
+      showSliderThumbsButton.classList.add("active");
+    }
+  });
 });
--- js/swiper.js
+++ js/swiper.js
@@ -1,68 +1,68 @@
 const swiper = new Swiper(".swiper", {
-	// Optional parameters
-	direction: "horizontal",
-	loop: true,
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
 
-	// If we need pagination
-	pagination: {
-		el: ".swiper-pagination",
-	},
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
 
-	// Navigation arrows
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
-	slidesPerView: 1,
-	breakpoints: {
-		320: {
-			slidesPerView: 2
-		}
-	}
+  slidesPerView: 1,
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+    },
+  },
 });
 
 const calendarSwiper = new Swiper(".calendar-swiper", {
-	// Optional parameters
-	direction: "horizontal",
-	loop: true,
-	// If we need pagination
-	pagination: {
-		el: ".swiper-pagination",
-	},
-	// Navigation arrows
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
-	controller: true,
-	slidesPerView: 2,
-	breakpoints: {
-		320: {
-			slidesPerView: 2
-		}
-	}
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  controller: true,
+  slidesPerView: 2,
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+    },
+  },
 });
 
-const galleryTop = new Swiper('.gallery-top', {
-	spaceBetween: 10,
-	navigation: {
-	  nextEl: '.swiper-button-next',
-	  prevEl: '.swiper-button-prev',
-	},
-	loop: true,
-	loopedSlides: 4,
-	slidesPerView: 1
+const galleryTop = new Swiper(".gallery-top", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  loop: true,
+  loopedSlides: 4,
+  slidesPerView: 1,
 });
 
-const galleryThumbs = new Swiper('.gallery-thumbs', {
-	spaceBetween: 10,
-	centeredSlides: true,
-	slidesPerView: 'auto',
-	touchRatio: 0.2,
-	slideToClickedSlide: true,
-	loop: true,
-	loopedSlides: 4
+const galleryThumbs = new Swiper(".gallery-thumbs", {
+  spaceBetween: 10,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  touchRatio: 0.2,
+  slideToClickedSlide: true,
+  loop: true,
+  loopedSlides: 4,
 });
 
 galleryTop.controller.control = galleryThumbs;
